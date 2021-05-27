b99b08d3bea542e9013edc4ec27ad5268ce49225
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-27 10:37:08 -0300

    Criando a url amigavel parte 3

--- app/core/Core.php
+++ app/core/Core.php
@@ -1,23 +1,46 @@
 <?php
 
 class Core {
+    private $controller;
+    private $metodo;
+    private $parametros = array();
 
     //Funcao que inicia o sistemas
     public function run() {
-        $url = explode("index.php", $_SERVER["PHP_SELF"]); //Retorna uma matriz de strings, cada uma como substring de string formada pela divisão dela a partir do delimiter.
+        $url = explode("index.php", $_SERVER["PHP_SELF"]); //explode - Retorna uma matriz de strings, cada uma como substring de string formada pela divisão dela a partir do delimiter.
         $url = end($url);
 
 
-        if (!$url == "") {
+        if (!$url == "") { //Verifica se a  $url não esta vazia
             $url = explode('/', $url);
             array_shift($url);
- 
+            
+            //Pega o controller
+            $this->controller = $url[0];
+            array_shift($url);          
+            
+            //Pega os metados
+            $this->metodo = $url[0];
+            array_shift($url);
+            
+            //Pega os parametros
+            $this->parametros = array_filter($url);//Esse aqui recebe todos os valores do array
         }
-
-        echo"<pre>";
-        echo $_SERVER["PHP_SELF"];
-        print_r($url);
-        echo"</pre>";
     }
+    
+    //Metados especiais 
+    function getController() {
+        return $this->controller;
+    }
+
+    function getMetodo() {
+        return $this->metodo;
+    }
+
+    function getParametros() {
+        return $this->parametros;
+    }
+
+
 
 }
--- index.php
+++ index.php
@@ -4,3 +4,14 @@ require("app/core/Core.php");//Chamando a classe Core
 
 $core=new Core();
 $core->run();//Executa run
+
+//Imprimindo na tela o valores dos indices do array $url da classe core
+echo"<pre>";
+echo "</br>Controler: ". $core->getController();
+echo "</br>Metado: ". $core->getMetodo();
+
+$parametro = $core->getParametros();
+foreach ($parametro as $param){//Varre o array e coloca em $param
+    echo "</br>Parametro: ".$param;
+}
+echo"</pre>";
