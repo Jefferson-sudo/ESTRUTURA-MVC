5b49bce978db4a883658ffc1584081bec1cb50a0
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-31 14:57:12 -0300

    Adicionando o arquivo config com as constantes

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('CONTROLLER_PADRAO', 'index');
+define('METADO_PADRAO', 'index');
+define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
+
+define('URL_BASE', 'http://localhost/MVC/');
+
